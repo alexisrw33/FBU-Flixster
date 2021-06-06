45d141baf4c158077e978e0797d951599d922538
alexisrw33 <69729270+alexisrw33@users.noreply.github.com>
2021-06-06 12:19:46 -0500

    Initial Commit

---
+++ FBU-Flixster/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  FBU-Flixster
+//
+//  Created by Alexis Rojas-Westall on 6/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FBU-Flixster/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FBU-Flixster
+//
+//  Created by Alexis Rojas-Westall on 6/6/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
